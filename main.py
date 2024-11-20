d45f8ad36f489a43e6507f1dd69506b41c941ea6
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-11-20 11:38:02 +0300

    delete_Nan

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Загрузка данных
-file_path = 'london_weather.csv'  # Замените на ваш путь к файлу
+file_path = 'london.csv'  # Замените на ваш путь к файлу
 df = pd.read_csv(file_path)
 
 # Просмотр данных
 print(df.head())
 print(df.info())
+
+# Преобразуем необходимые столбцы в числовой формат
+df['precipitation'] = pd.to_numeric(df['precipitation'], errors='coerce')
+df['max_temp'] = pd.to_numeric(df['max_temp'], errors='coerce')
+df['mean_temp'] = pd.to_numeric(df['mean_temp'], errors='coerce')
+df['min_temp'] = pd.to_numeric(df['min_temp'], errors='coerce')
+
+# Удаляем строки с пропущенными значениями в важнейших столбцах
+df_clean = df.dropna(subset=['precipitation', 'max_temp', 'mean_temp', 'min_temp'])
+
+# Проверим информацию о чистых данных
+print("\nОбщая информация о чистых данных:")
+print(df_clean.info())
